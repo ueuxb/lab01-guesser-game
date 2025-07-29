a26d13ec1f61b3fad0565d6a4e420804071dfb38
BoomPB <boonkerdma_p@silpakorn.edu>
2025-07-29 11:10:32 +0700

    checkandplay.py

--- game.py
+++ game.py
@@ -21,8 +21,13 @@ def check_guess(secret_number, player_guess):
     3. If the guess is too high, return the string "high".
     4. If the guess is too low, return the string "low".
     """
-    # Student 2: Add your code here
-    pass
+     if player_guess == secret_number:
+       return "correct"
+   elif player_guess > secret_number:
+       return "high"
+   else:
+       return "low"
+
 
 def play_game():
     """
@@ -32,23 +37,19 @@ def play_game():
     print("--- Welcome to the Number Guessing Game! ---")
     print("I'm thinking of a number between 1 and 100.")
 
-    # This part will be uncommented after the tasks are complete
-    # import random
-    # secret_number = random.randint(1, 100)
-    # attempts = 0
-    #
-    # while True:
-    #     attempts += 1
-    #     guess = get_player_guess()
-    #     result = check_guess(secret_number, guess)
-    #
-    #     if result == "correct":
-    #         print(f"Congratulations! You guessed the number in {attempts} attempts.")
-    #         break
-    #     elif result == "high":
-    #         print("Too high! Try again.")
-    #     elif result == "low":
-    #         print("Too low! Try again.")
+   secret_number = random.randint(1, 100)
+   attempts = 0
+   while True:
+       attempts += 1
+       guess = get_player_guess()
+       result = check_guess(secret_number, guess)
+       if result == "correct":
+           print(f"Congratulations! You guessed the number in {attempts} attempts.")
+           break
+       elif result == "high":
+           print("Too high! Try again.")
+       elif result == "low":
+           print("Too low! Try again.")
 
 if __name__ == "__main__":
     play_game()
